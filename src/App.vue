fa1060cf8b24d9f061ca38675e04796230217932
Alexander <58055542+IMrProYTI@users.noreply.github.com>
2024-06-13 18:27:06 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="flex justify-around *:*:box-content h-36">
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div>
+  <div class="*:my-2 *:*:my-2">
+    <RouterView></RouterView>
+  </div>
+  <p class="my-2">
+    <strong>Current route path:</strong> {{ $route.fullPath }}
+  </p>
+  <nav class="space-x-2">
+    <RouterLink to="/">Go to Home</RouterLink>
+    <RouterLink to="/about">Go to About</RouterLink>
+  </nav>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
