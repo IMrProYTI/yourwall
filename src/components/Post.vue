481e76787ef25e0e0b93729ddef3a0fd2d0ddea7
Alexander <s555b@mail.ru>
2024-06-13 22:57:29 +0300

    More post style

--- src/components/Post.vue
+++ src/components/Post.vue
@@ -11,14 +11,13 @@ import { IPost } from './Post';
 const props = defineProps<{post: IPost}>();
 
 const date = new Date(props.post.created_at).toLocaleString().replace(',', '').split(':').slice(0, -1).join(':')
-
-// {
-//   id: number;
-//   content: string[];
-//   created_at: string;
-// }
 </script>
 
 <style scoped>
-
+h1 { font-size: xx-large;}
+h2 { font-size: x-large;}
+h3 { font-size: larger;}
+h4 { font-size: large;}
+h5 { font-size: medium;}
+h6 { font-size: small;}
 </style>
